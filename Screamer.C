9d8c877f086d9eddf40ae9f58febbfe55e358ef1
Nathan McCausland <naphidmc@gmail.com>
2019-07-05 18:03:07 -0700

    Implement wireless functionality

--- Screamer.C
+++ Screamer.C
@@ -14,14 +14,14 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
 };
 
 byte rowPins[KEYPAD_ROWS] = {2, 14, 15, 16}; 
-byte colPins[KEYPAD_COLS] = {17, 18, 19, 20}; 
+byte colPins[KEYPAD_COLS] = {17, 4, 5, 20}; 
 Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS); 
 
 // LCD Setup
 const int rs = 9, en = 8, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int alarmTriggerPin = 51, buzzerPin = 50;
+const int alarmTriggerPin = 51, sirenPin = 50;
 
 const int CODE_LENGTH = 5, CURRENT_MESSAGE_DURATION = 3000;
 bool armed = false, currTextFull = false, messageActive = false, passwordChangeInProgress = false, validateNewCode = false,
@@ -62,13 +62,15 @@ void setCurrentMessage(const char message[])
 
 void setup(){
   Serial.begin(9600);
-  pinMode(alarmTriggerPin, INPUT);
-  pinMode(buzzerPin, OUTPUT);
-   // set up the LCD's number of columns and rows:
+  Serial1.begin(115200); // Baud rate for wifi chip which is connected to serial1
+  pinMode(alarmTriggerPin, INPUT_PULLUP); // Controls door switch
+  pinMode(sirenPin, OUTPUT); // Flashing light and siren
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   
   bool readPasscode = true;
   // If EEPROM has never been written to, tell the program to not read the passcode from memory
+  // Checked by seeing if memory address 0 contains the default value (255)
   if(EEPROM.read(0) == 255)
     readPasscode = false;
 
@@ -77,19 +79,33 @@ void setup(){
     newCode[i] = '\0';
     currText[i] = '\0';
 
-    // If passcode has never been saved, load defaul (all 0's)
+    // If passcode has never been saved, load default (5 0's)
     if(!readPasscode)
       code[i] = '0';
     else
       code[i] = (char) EEPROM.read(i);
   }
 
-  Serial.println(code);
+  Serial.println(code); // Prints the active passcode on startup to the computer
 }
-  
-void loop(){
+
+void setNewCode(const char c[])
+{
+  for(int i = 0; i < CODE_LENGTH; i++)
+  {
+    code[i] = c[i];
+    // Writes the new passcode to memory
+    EEPROM.write(i, (byte) c[i]);
+  }
+  setCurrentMessage("PASSCODE SET");
+}
+
+// Handles all keypad input
+void checkInput()
+{
   char keyPressed = customKeypad.getKey();
 
+  // Keypad input is disabled while a message is being displayed
   if (keyPressed && !messageActive){
     if(!currTextFull)
     {
@@ -136,13 +152,7 @@ void loop(){
           // Codes match, set new passcode
           if(codesMatch)
           {
-            for(int i = 0; i < CODE_LENGTH; i++)
-            {
-              code[i] = newCode[i];
-              // Writes the new passcode to memory
-              EEPROM.write(i, (byte) newCode[i]);
-            }
-            setCurrentMessage("PASSCODE SET");
+            setNewCode(newCode);
           }
           else
           {
@@ -160,16 +170,24 @@ void loop(){
       {
         if(validateCode(currText)) // Checks if the code entered is correct before proceeding to alarm functions
         {
-          alarmSounding = false;
-          // 'A' is pressed - Arm or Disarm
-          if(keyPressed == 'A')
+          // If the alarm is sounding, disable the alarm
+          if(alarmSounding)
           {
-            armed = !armed;
+            alarmSounding = false;
+            armed = false;
           }
-          // '*' is pressed - initiate code change
-          else if(keyPressed == '*')
+          else // Otherwise do normal alarm functions depending on what key is pressed 
           {
-            passwordChangeInProgress = true;
+            // 'A' is pressed - Arm or Disarm
+            if(keyPressed == 'A')
+            {
+              armed = !armed;
+            }
+            // '*' is pressed - initiate code change
+            else if(keyPressed == '*')
+            {
+              passwordChangeInProgress = true;
+            }
           }
         }
         else
@@ -191,6 +209,10 @@ void loop(){
     }
   }
 
+}
+
+void updateLCD()
+{
   lcd.setCursor(0, 0);
   // Displays password change instructions on the first row
   if(passwordChangeInProgress)
@@ -208,12 +230,15 @@ void loop(){
     if(!armed)
       lcd.print("NOT ");
     lcd.print("ARMED");
+
+    if(alarmSounding)
+      lcd.print(" (SOUNDING)");
   }
   
   lcd.setCursor(0, 1);
   if(messageActive)
   {
-    if(millis() - currentMessageStart >= CURRENT_MESSAGE_DURATION)
+    if(millis() - currentMessageStart >= CURRENT_MESSAGE_DURATION) // Message has expired, get rid of it
     {
       messageActive = false;
       for(int i = 0; i < 16; i++)
@@ -234,12 +259,79 @@ void loop(){
     }
   }
 
-  // Checks if alarm trigger mechanism is triggered
-  if(digitalRead(alarmTriggerPin) && armed)
-    alarmSounding = true;
+}
 
+void checkAlarmTriggered()
+{
+    
+  // Checks if alarm trigger mechanism is triggered
+  if(digitalRead(alarmTriggerPin) && armed && !alarmSounding)
+  {
+    Serial1.println("trig"); // Arduino tells the esp which tells the server that the alarm went off
+    alarmSounding = true;
+  }
+
+  // If the alarm is going off make an annoying buzzing sound
   if(alarmSounding)
   {
-    tone(buzzerPin, 500, 500);
+    digitalWrite(sirenPin, LOW); // Siren on
   }
+  else
+    digitalWrite(sirenPin, HIGH); // Siren is off
+}
+
+// Checks if the wifi module is trying to communicate
+void checkEspInput()
+{
+  char message[12] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
+  int index = 0;
+  // While there is input available read it into a char array
+  while(Serial1.available() > 0)
+  {
+    message[index] = (char) Serial1.read();
+    index++;
+  }
+  
+  // Parse input here
+  if(message[0] != '\0') // Something was read
+  {
+    // Check codes here
+    // TODO: Why are these codes 2 digits anyways? there wont be more than 10 commands? 
+    if(message[0] == '6' && message[1] == '5') // Code to arm
+    {
+      armed = true;
+      setCurrentMessage("R: Armed");
+    }
+    else if(message[0] == '7' && message[1] == '5') // Code to disarm
+    {
+      armed = false;
+      setCurrentMessage("R: Disarmed");
+    }
+    else if(message[0] == '5' && message[1] == '5') // Code to initiate passcode change
+    {
+       passwordChangeInProgress = false;
+       char cd[CODE_LENGTH];
+       for(int i = 0; i < CODE_LENGTH; i++)
+         cd[i] = message[3 + i];
+       setNewCode(cd);
+    }
+    else if(message[0] == '4' && message[1] == '5') // Sound alarm
+    {
+      alarmSounding = true;
+      setCurrentMessage("R: Sounding");
+    }
+    else if(message[0] == '3' && message[1] == '5') // Mute alarm 
+    {
+      alarmSounding = false;
+      armed = false;
+      setCurrentMessage("R: Muted");
+    }
+  }
+}
+
+void loop(){
+  checkEspInput();
+  checkInput();
+  updateLCD();
+  checkAlarmTriggered();
 }
