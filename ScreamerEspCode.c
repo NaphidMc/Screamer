391228e11115507abee4675767dbaccbc80644fe
Nathan McCausland <naphidmc@gmail.com>
2019-07-19 09:27:23 -0700

    Update ScreamerEspCode.c

--- ScreamerEspCode.c
+++ ScreamerEspCode.c
@@ -1,95 +1,180 @@
-/*
- * Wifi Module Code
- */
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-
-WiFiClient espClient; // Wifi client used to connect to servers
-
-void setup()
-{
-  Serial.begin(115200);
-  delay(10);
+  /*
+     Wifi Module Code
+  */  
+  #include <ESP8266WiFi.h>
+  #include <WiFiClient.h>
   
-  pinMode(2, OUTPUT);
-
-  // Serial.println("Starting");
-  WiFi.begin("Cottage", "mojomojo11");
-
-  // Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
+  WiFiClient espClient; // Wifi client used to connect to servers
+  
+  #define CONNECT_TIMEOUT 10000
+    
+  char ssid[33], pass[63];
+  bool tryConnect = false, attemptingCredentials = false;
+  
+  unsigned long lastOnCommand = -1, lastServerPing = 0;
+  
+  void setup()
   {
-    delay(500);
-    // Serial.print(".");
+    Serial.begin(74880);
+    WiFi.mode(WIFI_STA);
+    WiFi.setAutoConnect(false);
+    delay(10);
+    
+    pinMode(2, OUTPUT);
+    Serial.println("ON");
   }
-  // Serial.println();
-
-  // Serial.print("Connected, IP address: ");
-  // Serial.println(WiFi.localIP());
-}
-
-void loop() 
-{
-  // Flash pin 2 on the wifi module to indicate the program is running
-  /*digitalWrite(2, HIGH);
-  delay(500); 
-  digitalWrite(2, LOW);
-  delay(500);*/ 
-
-  // If not connected to server, try to connect
-  if(!espClient.connected())
+  
+  void loop()
   {
-    // Serial.println("Connecting to alarm server...");
-    if(!espClient.connect("192.168.1.10", 8080))
+    yield();
+    char inputBuffer[33];
+    int charactersReceived = 0;
+    
+    if(!WiFi.isConnected() && millis() - lastOnCommand > 5000 && !attemptingCredentials)
     {
-      // Serial.println("Failed to connect to alarm server");
+      Serial.println("ON");
+      lastOnCommand = millis();
+    }
+    
+    if(Serial.available())
+    {
+      yield();
+      char c = '\n';
+      do
+      {
+        if(Serial.available())
+        {
+          yield();
+          c = (char) Serial.read();
+          inputBuffer[charactersReceived] = c;
+          charactersReceived++;
+        }
+        yield();
+      } while(c != '\n' && c != '\r' && charactersReceived <= 33);
+      inputBuffer[charactersReceived - 1] = NULL; // Set last character to null
+      Serial.println(inputBuffer);
+    }
+  
+  if (charactersReceived > 0 && inputBuffer[0] == '#')
+  {
+    Serial.println("Esp read for SSID: ");
+    for(int i = 0; i < charactersReceived; i++)
+    {
+      ssid[i] = inputBuffer[i + 1];
+    }
+    Serial.println(ssid);
+    yield();
+    return;
+  }
+  else if(charactersReceived > 0 && inputBuffer[0] == '$')
+  {
+    Serial.println("Esp read for password: ");
+    for(int i = 0; i < charactersReceived; i++)
+    {
+      pass[i] = inputBuffer[i + 1];
+    }
+    Serial.println(pass);
+    yield();
+    tryConnect = true;
+    attemptingCredentials = true;
+    return;
+  }
+  
+  if(tryConnect)
+  {
+    Serial.println("Conecting to wifi...");
+    unsigned long startTime = millis();
+    WiFi.begin(ssid, pass); // Tries to connect with credentials
+    while (WiFi.waitForConnectResult() != WL_CONNECTED && millis() - startTime < CONNECT_TIMEOUT)
+    {
+      delay(500);
+    }
+
+    tryConnect = false;
+    attemptingCredentials = false;
+    if(WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("Connected");
+    }
+    else
+    {
+      // Reset variables so credentials can be reentered
+      Serial.print("Failed ");
+      tryConnect = false;
+      Serial.println(WiFi.status());
+      WiFi.disconnect();
+    }
+  }
+  
+  // If not connected to server, try to connect
+  if (WiFi.status() == WL_CONNECTED && !espClient.connected())
+  {
+    Serial.println("Connecting to alarm server...");
+    if (!espClient.connect("192.168.1.10", 8080))
+    {
+      Serial.println("Failed to connect to alarm server");
+      yield();
       return;
     }
-    // Serial.println("Success!");
-    espClient.println("esp");
+    Serial.println("Success!");
+    espClient.print("ESP ");
+    espClient.println(WiFi.macAddress());
   }
-  else
+  else if(WiFi.status() == WL_CONNECTED && espClient.connected())
   {
+    if(millis() - lastServerPing > 5000)
+    {
+      espClient.print("ESP ");
+      espClient.println(WiFi.macAddress());
+      lastServerPing = millis();
+    }
+    
     // While there client is receiving info over wifi
     char wifiMessage[12] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
-    int index = 0;
-    // If there is data to be read from the server
-    while(espClient.available() > 0)
+    charactersReceived = 0;
+
+    // Reads data from the server
+    if(espClient.available())
     {
-      char c = (char) espClient.read();
-      wifiMessage[index] = c;
-      index++;
-      // Toggle pin 2 upon receiving L or H, this is useful to test if esp is connected on it's own
-      if(c == 'H')
-        digitalWrite(2, HIGH);
-      else if(c == 'L')
-        digitalWrite(2, LOW);
+      yield();
+      char c = '\n';
+      do
+      {
+        if(espClient.available())
+        {
+          yield();
+          c = (char) espClient.read();
+          wifiMessage[charactersReceived] = c;
+          charactersReceived++;
+        }
+        yield();
+      } while(c != '\n' && c != '\r' && charactersReceived <= 12);
+      wifiMessage[charactersReceived - 1] = NULL; // Set last character to null
     }
 
     // Send wifi message recieved from the server to arduino
-    for(int i = 0; i < 12; i++)
-      if(wifiMessage[i] != '\0')
+    for (int i = 0; i < 12; i++)
+      if (wifiMessage[i] != '\0')
         Serial.print(wifiMessage[i]);
+    if(wifiMessage[0] != '\0')
+      Serial.println();
 
-    // Checks for input from arduino and sends it to the server
-    char arduinoMessage[12] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
-    if(Serial.available() > 4)
+    if(wifiMessage[1] == '\0')
     {
-      index = 0;
-      while(Serial.available() > 0)
-      {
-        char c = (char) Serial.read();
-        arduinoMessage[index] = c;
-        Serial.print(c);
-        index++;
-      }
+      if(wifiMessage[0] == 'H')
+        digitalWrite(2, HIGH);
+      else if(wifiMessage[0] == 'L')
+        digitalWrite(2, LOW);
     }
     
-    if(arduinoMessage[0] != '\0')
+    // Checks for input from arduino and sends it to the server
+    if (inputBuffer[0] == '>')
     {
-      Serial.println();
-      espClient.println(arduinoMessage);
+      espClient.print("ESP ");
+      espClient.print(WiFi.macAddress());
+      espClient.print(" ");
+      espClient.println(inputBuffer);
       // espClient.println("Hello World");
     }
   }
-}
+} 
